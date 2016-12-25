c2787b5c237f124850028d81f407b34a2dfbf043
liny90 <linly90@mail.ru>
2016-12-25 12:55:57 +0200

    add  items block

---
+++ game/create_Items.js
@@ -0,0 +1,36 @@
+
+function Item (name, src, usemap, pozX, pozY){
+    this.name = name;
+    this.src= src;
+    this.usemap = usemap;
+    this.drawImg = function(){
+        var img = document.createElement('img');
+        img.src = this.src;
+        img.name = this.name;
+        img.setAttribute('usemap', this.usemap);
+        document.getElementById('contentMain').appendChild(img);
+        img.style.left = pozX;
+        img.style.top = pozY;
+        imgMainName.push(img.name)
+    }
+}
+
+
+    var cat = new Item ("cat", "images/cat.png", "#cat","150", "370");
+    cat.drawImg();
+
+
+    var dreamCatcher = new Item ("dreamCatch", "images/dreamsCatcher.png", "#dreamCatch","650", "20");
+    dreamCatcher.drawImg();
+
+    var jar = new Item ("jar", "images/pitcher.png", "#jar","800", "480");
+    jar.drawImg();
+
+    var plane = new Item ("plane", "images/plane.png", "#plane","230", "45");
+    plane.drawImg();
+
+    var worm = new Item ("worm", "images/worm.png", "#worm","400", "385");
+    worm.drawImg();
+
+
+
--- game/game.js
+++ game/game.js
@@ -9,49 +9,6 @@
 //imgMain = Array.prototype.slice.call(imgMain);
 
 
-
-
-
-
-
-
-
-function Item (name, src, usemap, pozX, pozY){
-    this.name = name;
-    this.src= src;
-    this.usemap = usemap;
-    this.drawImg = function(){
-        var img = document.createElement('img');
-        img.src = this.src;
-        img.name = this.name;
-        img.setAttribute('usemap', this.usemap);
-        document.getElementById('contentMain').appendChild(img);
-        img.style.left = pozX;
-        img.style.top = pozY;
-        imgMainName.push(img.name)
-    }
-}
-
-
-    var cat = new Item ("cat", "images/cat.png", "#cat","150", "370");
-    cat.drawImg();
-
-
-    var dreamCatcher = new Item ("dreamCatch", "images/dreamsCatcher.png", "#dreamCatch","650", "20");
-    dreamCatcher.drawImg();
-
-    var jar = new Item ("jar", "images/pitcher.png", "#jar","800", "480");
-    jar.drawImg();
-
-    var plane = new Item ("plane", "images/plane.png", "#plane","230", "45");
-    plane.drawImg();
-
-    var worm = new Item ("worm", "images/worm.png", "#worm","400", "385");
-    worm.drawImg();
-
-
-
-
     var imgMain = document.querySelectorAll('#contentMain img');
 
 
