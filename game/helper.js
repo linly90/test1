f86c6fd934b9857317b4b5ceba3a8f5c11208b37
linly90 <linly90@mail.ru>
2017-01-04 19:39:43 +0200

    add helper

--- game/helper.js
+++ game/helper.js
@@ -2,7 +2,10 @@
 document.querySelector("#helper")
         .addEventListener("click", function(){
 
-           runHelper ();
+
+                runHelper ();
+
+
         });
 
 
@@ -25,11 +28,67 @@ function runHelper (){
 
 
 
-    currentImg.style.backgroundColor= "green";
+
+            createRotateHelper(currentImg);
+
+
+}
+function createRotateHelper(currentImg){
+
+
+
+    var circleImg = document.createElement('img');
+    main.appendChild(circleImg);
+    circleImg.src = "images/blink2.png";
+    circleImg.id = 'rot';
+
+
+    circleImg.style.left =  parseInt(currentImg.style.left) -40 + 'px';
+    circleImg.style.top = parseInt(currentImg.style.top) -30 + 'px';
+
+    setTimeout(function() {
+        main.removeChild(circleImg) ;
+
+    }, 4000);
+
+}
+
+/*
+function runHelper (){
+    var currentImgBottom = document.querySelectorAll('#bottomPanel img');
+    var rand = getRandom(0, currentImgBottom.length-1);
+
+    var currentImgName = currentImgBottom[rand].name;
+    var currentImgNumber= imgMainName.indexOf(currentImgName);
+
+    var currentImg = imgMain[currentImgNumber];
+
+
+
+    function createRotateHelper(currentImg){
+
+
+        var divParent = document.createElement("div");
+        var parent = currentImg.parentNode;
+        parent.insertBefore(divParent, currentImg);
+        divParent.appendChild(currentImg);
+
+
+
+        var circleImg = document.createElement('img')
+        divParent.appendChild(circleImg);
+        circleImg.src = "images/blink2.png";
+        circleImg.id = 'rot';
+
+        console.log(divParent)
+
+
+
+    }
+    createRotateHelper(currentImg);
+    // currentImg.style.backgroundColor= "green";
 
 
 //console.log(currentImg)
 
-}
-
-
+}*/
